db27a2e2f8e773592f3d746d472df213811d3478
imamknitto <it14.knitto@gmail.com>
2025-10-13 15:00:28 +0700

    chore: header layout and color toggle

--- src/components/layout/theme-toggle.tsx
+++ src/components/layout/theme-toggle.tsx
@@ -6,7 +6,7 @@ const ThemeToggle = () => {
 
   return (
     <button
-      className='inline-flex items-center justify-center rounded-md border border-input bg-background p-2 text-sm font-medium shadow-sm hover:bg-accent hover:text-accent-foreground transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
+      className='cursor-pointer inline-flex items-center justify-center rounded-md border border-input bg-white dark:bg-black/80 p-2 text-sm font-medium shadow-sm hover:bg-accent hover:text-accent-foreground transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
       onClick={toggleTheme}
       type='button'
       aria-label='Toggle theme'
