626cdb6e78d0860c4ee7cbfe4381b6e93cc77cab
imamknitto <it14.knitto@gmail.com>
2025-10-13 16:10:22 +0700

    update vite config and route basepath

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/virtual-table/',
   server: { port: 3002 },
   build: {
     rollupOptions: {
